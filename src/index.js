51fbf84ee1372e4ae111fc42b020286ba5d09c83
Jakhongir Nishanbaev <jakhongir.nishanbaev@gmail.com>
2019-07-05 16:52:15 +0500

    Solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 module.exports = function warmup(temperature) {
-  if (temperature === -20) {
-    return -4;
-  }
-  if (temperature === 0) {
-    return 32;
-  }
+    if (temperature === -20) {
+        return -4;
+    }
+    if (temperature === 0) {
+        return 32;
+    }
+
+    let fahrenheit = 9/5 * temperature + 32;
+
+    return fahrenheit;
 };
